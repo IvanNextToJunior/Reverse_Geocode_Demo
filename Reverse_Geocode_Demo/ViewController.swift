49ea076dc70457f3ff23dc81656aa0d1b6462cc5
IvanNextToJunior <Vanwan@bk.ru>
2022-11-21 21:37:58 +0300

    setup ui

--- Reverse_Geocode_Demo/ViewController.swift
+++ Reverse_Geocode_Demo/ViewController.swift
@@ -6,9 +6,18 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+ 
 
+@IBOutlet weak var mapView: MKMapView!
+@IBOutlet weak var geocodeLabel: UILabel!
+   
+    @IBAction func reverseGeocodeButtonTouchUpInside(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
