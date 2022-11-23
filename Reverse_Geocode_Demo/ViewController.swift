78f8939e7fd254abed2f11ed7343d2e656b7a7b8
IvanNextToJunior <Vanwan@bk.ru>
2022-11-23 19:45:02 +0300

    Update ViewController.swift

--- Reverse_Geocode_Demo/ViewController.swift
+++ Reverse_Geocode_Demo/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var pinIcon: UIImageView!
     
     @IBAction func reverseGeocodeButtonTouchUpInside(_ sender: UIButton) {
+    let coordinate = locationAtcenterOfMapView()
+    let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    startReverseGeocodeLocation(location: location)
     }
     
     var geocoder: CLGeocoder!
@@ -29,6 +32,46 @@ class ViewController: UIViewController {
         
     }
     
+    func locationAtcenterOfMapView () -> CLLocationCoordinate2D {
+        let centerOfPin = CGPoint(x: pinIcon.bounds.midX, y: pinIcon.bounds.midY)
+        return mapView.convert(centerOfPin, toCoordinateFrom: pinIcon)
+        
+    }
     
+    func startReverseGeocodeLocation(location: CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+           
+            if error != nil {
+        
+                let alert = UIAlertController(title: "There was a problem reverse geocoding", message: error!.localizedDescription, preferredStyle: .alert)
+                self.present(alert, animated: true, completion: nil)
+            }
+      
+            let mappedPlacesDescriptions = NSMutableSet()
+            
+            guard let placemarks = placemarks else {return}
+           
+            for placemark in placemarks {
+               
+                if placemark.name != nil {
+                    mappedPlacesDescriptions.add(placemark.name!)
+                }
+          
+                if placemark.administrativeArea != nil {
+                    mappedPlacesDescriptions.add(placemark.administrativeArea!)
+                }
+                
+                if placemark.country != nil {
+                    mappedPlacesDescriptions.add(placemark.country!)
+                    mappedPlacesDescriptions.addObjects(from: placemark.areasOfInterest!)
+               }
+               
+              
+                self.geocodeLabel.text = (mappedPlacesDescriptions.allObjects as! [String]).joined(separator: "\n")
+               self.geocodeLabel.alpha = 1.0
+            }
+        
+        }
+    }
 }
 
